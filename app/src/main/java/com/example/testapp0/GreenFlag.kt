f72f4c29776102f3002c525f7b2d869a9ce91e34
Grizaq <77796055+Grizaq@users.noreply.github.com>
2022-08-30 14:22:03 +0100

    GreenFlag

---
+++ app/src/main/java/com/example/testapp0/GreenFlag.kt
@@ -0,0 +1,11 @@
+package com.example.testapp0
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GreenFlag : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_green_flag)
+    }
+}
